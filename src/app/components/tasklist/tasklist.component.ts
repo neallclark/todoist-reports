3e92c151b3af26acf9e432ff97a1433ae0042d8c
neallclark <neallclark@gmail.com>
2018-02-23 15:13:19 +0000

    some tweaks to the formatting of the tables

--- src/app/components/tasklist/tasklist.component.ts
+++ src/app/components/tasklist/tasklist.component.ts
@@ -9,4 +9,12 @@ export class TaskListComponent {
     @Input() tasks: Array<Task>;
 
     constructor(){}
+
+    formatDate(date: string) : string{
+        if(date == null || date.length == 0)
+            return "";
+
+        let d = new Date(date);
+        return d.toDateString();
+    }
 }
